146c06a506f198ca14cd15a578a28c97d552fd7a
“Ha <ngpha@yahoo.com>
2024-02-04 23:07:42 +0700

    add docker-compose

---
+++ README.md
@@ -0,0 +1,9 @@
+`python manage.py migrate`
+
+`python3 manage.py makemigrations`
+
+`python3 manage.py runserver`
+
+`source env/bin/activate`
+
+`systemctl stop postgresql`
--- pinchi/ecommerce/models/product.py
+++ pinchi/ecommerce/models/product.py
@@ -6,6 +6,7 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    
     objects = models.Manager()
     def __str__(self):
         return str(self.name)
